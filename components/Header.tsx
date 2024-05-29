9e4aa18894bf0642a9ec328624ac35d3717ba8bb
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2024-05-29 20:11:53 +0100

    done with list items component

--- components/Header.tsx
+++ components/Header.tsx
@@ -43,23 +43,35 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
           </button>
         </div>
         <div className="flex md:hidden gap-x-2 items-center">
-            <button className="rounded-full p-2 bg-white fle items-center hover:opacity-75 transition">
-                  <HiHome className="text-black" size={20}/>
-            </button>
-            <button className="rounded-full p-2 bg-white fle items-center hover:opacity-75 transition">
-                  <BiSearch className="text-black" size={20}/>
-            </button>
+          <button className="rounded-full p-2 bg-white fle items-center hover:opacity-75 transition">
+            <HiHome className="text-black" size={20} />
+          </button>
+          <button className="rounded-full p-2 bg-white fle items-center hover:opacity-75 transition">
+            <BiSearch className="text-black" size={20} />
+          </button>
         </div>
         <div className="flex justify-between items-center gap-x-2">
-            <>
-                  <div>
-                        <Button>
-                              Sign up
-                        </Button>
-                  </div>
-            </>
+          <>
+            <div>
+              <Button
+                onClick={() => {}}
+                className="bg-transparent text-neutral-300 font-medium"
+              >
+                Sign up
+              </Button>
+            </div>
+            <div>
+              <Button 
+                onClick={() => {}} 
+                className="bg-white px-6 py-2"
+              >
+                Log in
+              </Button>
+            </div>
+          </>
         </div>
       </div>
+      {children}
     </div>
   );
 };
