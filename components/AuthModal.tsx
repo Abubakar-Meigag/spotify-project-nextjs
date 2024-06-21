cf3ee67cafa9b0ab026244def2579f156a325854
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2024-06-21 16:43:46 +0100

    update create login auth with supabase

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -1,17 +1,51 @@
 'use client';
 
+import { useSessionContext, useSupabaseClient } from "@supabase/auth-helpers-react";
+
+import useAuthModal from '@/hooks/useAuthModal'
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { Auth } from "@supabase/auth-ui-react";
+import { useRouter } from "next/navigation";
 import Modal from "./Modal";
+
 
  
  const AuthModal = () => {
+  const supabaseClient = useSupabaseClient();
+  const router = useRouter();
+  const { session } = useSessionContext();
+  const { onClose, isOpen } = useAuthModal();
+
+  const onChange = (open: boolean) => {
+    if (!open) {
+      onClose();
+    }
+  }
+
    return (
      <Modal
       title="Welcome back"
       description="Login to your account"
-      isOpen
-      onChange={() => {}}
+      isOpen={isOpen}
+      onChange={onChange}
      >
-        Auth modal children! 
+        <Auth 
+          theme="dark"
+          magicLink
+          providers={[ 'google', 'apple','github']}
+          supabaseClient={supabaseClient}
+          appearance={{
+            theme: ThemeSupa,
+            variables: {
+              default: {
+                colors: {
+                  brand: '#404040',
+                  brandAccent: '#00a550'
+                }
+              }
+            }
+          }}
+        />
      </Modal>
    )
  }
--- components/Header.tsx
+++ components/Header.tsx
@@ -6,6 +6,7 @@ import { HiHome } from "react-icons/hi";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { twMerge } from "tailwind-merge";
 import Button from "./Button";
+import useAuthModal from "@/hooks/useAuthModal";
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -13,6 +14,7 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ children, className }) => {
+  const authModal = useAuthModal();
   const router = useRouter();
   const handleLogout = () => {
     // Handle logout
@@ -54,7 +56,7 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
           <>
             <div>
               <Button
-                onClick={() => {}}
+                onClick={authModal.onOpen}
                 className="bg-transparent text-neutral-300 font-medium"
               >
                 Sign up
@@ -62,7 +64,7 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
             </div>
             <div>
               <Button 
-                onClick={() => {}} 
+                onClick={authModal.onOpen} 
                 className="bg-white px-6 py-2"
               >
                 Log in
