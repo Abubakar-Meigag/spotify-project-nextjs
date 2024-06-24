f565cb3e01c739264bf3aa95aaf70e0cd69fe88e
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2024-06-24 19:31:25 +0100

    create upload component and add use upload modal

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,12 +1,23 @@
 "use client";
 
+import { useUser } from "@/hooks/uesUser";
+import useAuthModal from "@/hooks/useAuthModal";
+import useUploadModal from "@/hooks/useUploadModal";
 import { AiOutlinePlus } from "react-icons/ai";
 import { TbPlaylist } from "react-icons/tb";
 
 const Library = () => {
+  const authModal = useAuthModal();
+  const uploadModal = useUploadModal();
+  const { user } = useUser();
   const onClick = () => {
-    // handle upload
-  };
+    if(!user) {
+      return authModal.onOpen();
+    }
+
+    return uploadModal.onOpen();
+
+  }; 
   return (
     <div className="flex flex-col">
       <div className="flex items-center justify-between px-5 pt-4">
